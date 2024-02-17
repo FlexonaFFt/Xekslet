552db982b3288ff50f59e1418ddd6d3a9519d51d
FlexonaFFt <flexonafft@bk.ru>
2024-02-17 09:19:35 +0300

    second prog load

---
+++ JS/practice_3.js
@@ -0,0 +1,6 @@
+const squareOfSum = (first, second) => {
+    return first ** 2 + 2 * first * second + second ** 2; 
+};
+
+const formula = squareOfSum(2, 3);
+const second_formula = squareOfSum(1, 10);
